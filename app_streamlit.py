573f1d7efc6ce6da122db1f42a93bfcaaba03876
tetsuya-yamada <tetsuya.yamada@kikagaku.co.jp>
2025-03-23 21:16:03 +0900

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -31,7 +31,7 @@ elif scissors_button:
 
 # APIリクエスト
 if player_choice:
-    api_url = "http://localhost:8000/janken/" #　FastAPI は、デフォルトで localhost:8000
+    api_url = "https://janken-app.onrender.com/" #　FastAPI は、デフォルトで localhost:8000
     
     response = requests.post(
         api_url,
